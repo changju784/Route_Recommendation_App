0c76dce71f2d47ae0fb870b1167b1ed51d0054d9
minseoksakong <msakong@bu.edu>
2022-05-03 17:03:43 -0400

    added path checking algorithm

--- engines/build_graph.py
+++ engines/build_graph.py
@@ -7,11 +7,28 @@ from engines.Vertex import vertex
 import warnings
 warnings.filterwarnings("ignore")
 
+FILE_PATH = "dataset/Boston.graphml"
+
+def calc_latlong(Graph, lat, long):
+    target_xy = (lat, long)
+    target_node = ox.get_nearest_node(Graph, target_xy, method='euclidean')
+    return target_node
 
 def build_graph(start, end):
-    map = nx.read_graphml("dataset/Boston.graphml", node_type=vertex["osmid"])
+    map = nx.read_graphml(FILE_PATH, node_type=vertex["osmid"])
     map = ox.io._convert_node_attr_types(map, vertex)
     map = ox.io._convert_edge_attr_types(map, edge)
+    Gmap = ox.load_graphml(FILE_PATH)
+
+    #Test if there is a patch between start and end
+    start_node = calc_latlong(Gmap,start[0],start[1])
+    end_node = calc_latlong(Gmap,end[0],end[1])
+    if not nx.algorithms.shortest_paths.generic.has_path(Gmap, start_node, end_node):
+        print("There is no path between two points in Boston.")
+        exit(1)
+    else:
+        print("There is a path between two points.")
+        print("Calculating the path now....")
 
     if "node_default" in map.graph:
         del map.graph["node_default"]
@@ -20,6 +37,7 @@ def build_graph(start, end):
 
     Gp = ox.project_graph(map)
     Gc = ox.consolidate_intersections(Gp, rebuild_graph=True, tolerance=20, dead_ends=False)
+
 
     lats = [start[0], end[0]]
     longs = [start[1], end[1]]
