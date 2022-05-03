a5635a0a8d2c5285eb9d0a0ed6768669d8b9b472
ttpaik <ttpaik@bu.edu>
2022-05-03 15:19:34 -0400

    engine

--- engines/Edge.py
+++
@@ -1,6 +0,0 @@
-class Edge:
-    def __init__(self, name, start, end, weight):
-        self.name = name
-        self.start = start
-        self.end = end
-        self.weight = weight
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from engines.algorithms import BellmanFord
 from engines import Vertex, Edge, build_graph
 import osmnx as ox
-import networkx as nx
+from engines.algorithms import Dijkstras
+import warnings
+warnings.filterwarnings("ignore")
 
-def getShortestPath(src_lon, src_lat, dest_lon, dest_lat):
-    src = (src_lon, src_lat)
-    dest = (dest_lon, dest_lat)
-    graph, vts, idx_vt_dict, src_vt, dst_vt = build_graph.build_graph(src, dest)
-    route, directions, sum = BellmanFord.BellmanFord(vts, idx_vt_dict, src_vt, dst_vt)
-    nc = (0.976, 0.411, 0.411, 1.0)
-    background = (1.0, 1.0, 1.0, 0.0)
-    graph_proj = ox.project_graph(graph)
-    fig, ax = ox.plot_graph_route(graph_proj, route, node_color='w', node_size=0, edge_linewidth=0.5, route_color=nc,
-                                  bgcolor=background, show=False, save=True, filepath="map.png")
-    fig.savefig('result.png')
 
+def getShortestPath(Gc, source_node, target_node):
+    d_path, d_len = Dijkstras.dijkstra(Gc, source_node, target_node)
+    fig, ax = ox.plot_graph_route(Gc, d_path)
 
 if __name__ == '__main__':
-    getShortestPath(42.383807, -71.116494, 42.253763, -71.017757)
+    start = (42.3570104, -71.0710964)
+    end = (42.3568701, -71.0682476)
+    Gc, src, target = build_graph.build_graph(start, end)
+    print('==== Graph Loaded ====')
+    getShortestPath(Gc, src, target)
+    print('==== Complete ====')
